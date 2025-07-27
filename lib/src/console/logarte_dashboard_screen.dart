349f89d68f1b275550ef9aa01bd23f90c27dce26
Chayan Mistry <chayanmistrry@gmail.com>
2025-07-27 10:29:27 +0600

    add clear logs functionality

--- lib/src/console/logarte_dashboard_screen.dart
+++ lib/src/console/logarte_dashboard_screen.dart
@@ -9,10 +9,10 @@ class LogarteDashboardScreen extends StatefulWidget {
   final bool showBackButton;
 
   const LogarteDashboardScreen(
-    this.instance, {
-    Key? key,
-    this.showBackButton = false,
-  }) : super(key: key);
+      this.instance, {
+        Key? key,
+        this.showBackButton = false,
+      }) : super(key: key);
 
   @override
   State<LogarteDashboardScreen> createState() => _LogarteDashboardScreenState();
@@ -41,112 +41,125 @@ class _LogarteDashboardScreenState extends State<LogarteDashboardScreen> {
       child: DefaultTabController(
         length: widget.instance.customTab != null ? 6 : 5,
         child: Scaffold(
-          body: NestedScrollView(
-            headerSliverBuilder: (context, innerBoxIsScrolled) {
-              return [
-                SliverAppBar(
-                  floating: true,
-                  snap: true,
-                  leading: widget.showBackButton
-                      ? IconButton(
+          body: ValueListenableBuilder<List<LogarteEntry>>(
+              valueListenable: widget.instance.logs,
+              builder: (context, values, child) {
+                final allCount = values.length;
+                final loggingCount =
+                    values.whereType<PlainLogarteEntry>().length;
+                final networkCount =
+                    values.whereType<NetworkLogarteEntry>().length;
+                final databaseCount =
+                    values.whereType<DatabaseLogarteEntry>().length;
+                final navigationCount =
+                    values.whereType<NavigatorLogarteEntry>().length;
+
+                return NestedScrollView(
+                  headerSliverBuilder: (context, innerBoxIsScrolled) {
+                    return [
+                      SliverAppBar(
+                        floating: true,
+                        snap: true,
+                        leading: widget.showBackButton
+                            ? IconButton(
                           onPressed: () {
                             Navigator.of(context).pop();
                           },
-                          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                          icon: const Icon(
+                              Icons.arrow_back_ios_new_rounded),
                         )
-                      : null,
-                  automaticallyImplyLeading: false,
-                  title: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: 'Search',
-                      filled: true,
-                      prefixIcon: const Icon(Icons.search),
-                      suffixIcon: IconButton(
-                        icon: const Icon(Icons.clear),
-                        onPressed: _controller.clear,
-                      ),
-                    ),
-                  ),
-                  bottom: TabBar(
-                    isScrollable: true,
-                    tabAlignment: TabAlignment.center,
-                    labelPadding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    indicatorSize: TabBarIndicatorSize.tab,
-                    tabs: [
-                      Tab(
-                        icon: const Icon(Icons.list_alt_rounded),
-                        text: 'All (${widget.instance.logs.value.length})',
-                      ),
-                      Tab(
-                        icon: const Icon(Icons.bug_report_rounded),
-                        text:
-                            'Logging (${widget.instance.logs.value.whereType<PlainLogarteEntry>().length})',
-                      ),
-                      Tab(
-                        icon: const Icon(Icons.public),
-                        text:
-                            'Network (${widget.instance.logs.value.whereType<NetworkLogarteEntry>().length})',
-                      ),
-                      Tab(
-                        icon: const Icon(Icons.save_as_rounded),
-                        text:
-                            'Database (${widget.instance.logs.value.whereType<DatabaseLogarteEntry>().length})',
-                      ),
-                      Tab(
-                        icon: const Icon(Icons.navigation_rounded),
-                        text:
-                            'Navigation (${widget.instance.logs.value.whereType<NavigatorLogarteEntry>().length})',
-                      ),
-                      if (widget.instance.customTab != null)
-                        const Tab(
-                          icon: Icon(Icons.extension_rounded),
-                          text: 'Custom',
+                            : null,
+                        automaticallyImplyLeading: false,
+                        title: TextField(
+                          controller: _controller,
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                            filled: true,
+                            prefixIcon: const Icon(Icons.search),
+                            suffixIcon: IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: _controller.clear,
+                            ),
+                          ),
                         ),
-                    ],
-                  ),
-                ),
-              ];
-            },
-            // To rebuild the list when the logs list gets modified
-            body: ValueListenableBuilder(
-              valueListenable: widget.instance.logs,
-              builder: (context, values, child) {
-                return AnimatedBuilder(
-                  animation: _controller,
-                  builder: (_, __) {
-                    final search = _controller.text.toLowerCase();
-
-                    return TabBarView(
-                      children: [
-                        _List<LogarteEntry>(
-                          instance: widget.instance,
-                          search: search,
+                        bottom: TabBar(
+                          isScrollable: true,
+                          tabAlignment: TabAlignment.center,
+                          labelPadding:
+                          const EdgeInsets.symmetric(horizontal: 12.0),
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          tabs: [
+                            Tab(
+                              icon: const Icon(Icons.list_alt_rounded),
+                              text: 'All ($allCount)',
+                            ),
+                            Tab(
+                              icon: const Icon(Icons.bug_report_rounded),
+                              text: 'Logging ($loggingCount)',
+                            ),
+                            Tab(
+                              icon: const Icon(Icons.public),
+                              text: 'Network ($networkCount)',
+                            ),
+                            Tab(
+                              icon: const Icon(Icons.save_as_rounded),
+                              text: 'Database ($databaseCount)',
+                            ),
+                            Tab(
+                              icon: const Icon(Icons.navigation_rounded),
+                              text: 'Navigation ($navigationCount)',
+                            ),
+                            if (widget.instance.customTab != null)
+                              const Tab(
+                                icon: Icon(Icons.extension_rounded),
+                                text: 'Custom',
+                              ),
+                          ],
                         ),
-                        _List<PlainLogarteEntry>(
-                          instance: widget.instance,
-                          search: search,
-                        ),
-                        _List<NetworkLogarteEntry>(
-                          instance: widget.instance,
-                          search: search,
-                        ),
-                        _List<DatabaseLogarteEntry>(
-                          instance: widget.instance,
-                          search: search,
-                        ),
-                        _List<NavigatorLogarteEntry>(
-                          instance: widget.instance,
-                          search: search,
-                        ),
-                        if (widget.instance.customTab != null)
-                          widget.instance.customTab!,
-                      ],
-                    );
+                      ),
+                    ];
                   },
+                  // To rebuild the list when the logs list gets modified
+                  body: AnimatedBuilder(
+                    animation: _controller,
+                    builder: (_, __) {
+                      final search = _controller.text.toLowerCase();
+
+                      return TabBarView(
+                        children: [
+                          _List<LogarteEntry>(
+                            instance: widget.instance,
+                            search: search,
+                          ),
+                          _List<PlainLogarteEntry>(
+                            instance: widget.instance,
+                            search: search,
+                          ),
+                          _List<NetworkLogarteEntry>(
+                            instance: widget.instance,
+                            search: search,
+                          ),
+                          _List<DatabaseLogarteEntry>(
+                            instance: widget.instance,
+                            search: search,
+                          ),
+                          _List<NavigatorLogarteEntry>(
+                            instance: widget.instance,
+                            search: search,
+                          ),
+                          if (widget.instance.customTab != null)
+                            widget.instance.customTab!,
+                        ],
+                      );
+                    },
+                  ),
                 );
-              },
-            ),
+              }),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              widget.instance.clear();
+            },
+            child: const Icon(Icons.delete_forever_rounded),
           ),
         ),
       ),
@@ -169,7 +182,7 @@ class _List<T extends LogarteEntry> extends StatelessWidget {
 
     final filtered = logs.where((log) {
       return log.contents.any(
-        (content) => content.toLowerCase().contains(search),
+            (content) => content.toLowerCase().contains(search),
       );
     }).toList();
 
